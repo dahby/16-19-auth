e07c05459651ae23efb6929a26f461ae3ffee3bf
dahby <dstoll243@gmail.com>
2018-05-08 06:05:14 -0700

    first test passing

--- src/__test__/auth-router.test.js
+++ src/__test__/auth-router.test.js
@@ -1,1 +1,26 @@
 'use strict';
+
+import superagent from 'superagent';
+import { startServer, stopServer } from '../lib/server';
+import { removeAccountMock } from './lib/account-mock';
+
+const apiURL = `http://localhost:${process.env.PORT}/signup`;
+
+describe('auth-router', () => {
+  beforeAll(startServer);
+  afterAll(stopServer);
+  afterEach(removeAccountMock);
+
+  test('POST should return 200 & TOKEN', () => {
+    return superagent.post(apiURL)
+      .send({
+        username: 'david',
+        email: 'david@david.com',
+        password: 'davidspassword1',
+      })
+      .then((response) => {
+        expect(response.status).toEqual(200);
+        expect(response.body.token).toBeTruthy();
+      });
+  });
+});
--- src/__test__/lib/test.env.js
+++ src/__test__/lib/test.env.js
@@ -1,1 +1,4 @@
 process.env.NODE_ENV = 'development';
+process.env.PORT = 7000;
+process.env.MONGODB_URI = 'mongodb://localhost/testing';
+process.env.PDF_SECRET = '1234knasvc908ekqr238b8anwd5na98JxeMXfRbyblMQpfStx5gXZew3r2YX1kmCay2NNpR2mM4ujzsZoq';
